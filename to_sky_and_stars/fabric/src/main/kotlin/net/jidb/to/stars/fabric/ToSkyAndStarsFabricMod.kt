d7f754f63482547ed6bcb322eff5b3cc931ba789
Josh <josh@josh-leviathan>
2026-02-25 01:42:04 +0000

    Mod 2 enters the game (#3)
    So far.
    Moving away from Architectury.
    Using Kotlin Gradle and added Kotlin to both modloader projects.
    Reintroduced sky and stars successfully.
    Fixed building in fresh repositories.
    Moved including to_base/forge and to_base/fabric into a reusable function in multiloader-loader.
    Everything we ever dreamed of.

---
+++ to_sky_and_stars/fabric/src/main/kotlin/net/jidb/to/stars/fabric/ToSkyAndStarsFabricMod.kt
@@ -0,0 +1,23 @@
+package net.jidb.to.stars.fabric
+
+import net.fabricmc.api.ModInitializer
+import net.jidb.to.base.ToBaseMod
+import net.jidb.to.base.fabric.ToBaseFabricMod
+import org.slf4j.LoggerFactory
+import net.jidb.to.stars.ToSkyAndStarsMod
+
+object ToSkyAndStarsFabricMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("to_sky_and_stars")
+
+	override fun onInitialize() {
+		ToSkyAndStarsMod.init()
+
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+
+		ToBaseMod.logger.info("Hello ${ToBaseMod.mod_id} from fabric ${ToSkyAndStarsMod.mod_id}!")
+		ToBaseMod.logger.info("Base fabric mod class: $ToBaseFabricMod")
+	}
+}
